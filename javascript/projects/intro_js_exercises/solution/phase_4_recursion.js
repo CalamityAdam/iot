9374e0d9e6e8a3bab6fb6a66d7aa5f575adbce57
Aaron Wayne <amwayne90@gmail.com>
2018-01-09 17:57:58 -0800

    CUR-40 Add deep dup to js intro exercises

--- javascript/projects/intro_js_exercises/solution/phase_4_recursion.js
+++ javascript/projects/intro_js_exercises/solution/phase_4_recursion.js
@@ -93,6 +93,33 @@ function fibsIter(n) {
 
 console.log(`fibsIter(5) = ${fibsIter(5)}`);
 
+// deepDup
+
+// This function is a type-checking helper function
+// See this article for an explanation:
+// https://toddmotto.com/understanding-javascript-types-and-reliable-type-checking/
+function getType(el) {
+  return Object.prototype.toString.call(el).slice(8, -1);
+}
+
+function deepDup(arr) {
+  if (getType(arr) !== 'Array') {
+    return arr;
+  }
+  return arr.map((el) => {
+    return deepDup(el);
+  });
+}
+
+const array = [[2],3];
+const dupedArray = deepDup(array);
+console.log(`deepDup original = ${JSON.stringify(array)}`);
+
+dupedArray[0].push(4);
+
+console.log(`deepDup original = ${JSON.stringify(array)} (should not be mutated)`);
+console.log(`deepDup duped = ${JSON.stringify(dupedArray)} (should be mutated)`);
+
 // bsearch
 function bsearch(numbers, target) {
   if (numbers.length === 0) {
