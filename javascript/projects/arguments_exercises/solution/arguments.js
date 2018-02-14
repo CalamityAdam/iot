882358f31afef980c12560b07fdf9697b7c5722f
aalfarho <st.alfaro@gmail.com>
2018-02-13 16:32:02 -0800

    CUR-250 include arrow funtion solutio for currying

--- javascript/projects/arguments_exercises/solution/arguments.js
+++ javascript/projects/arguments_exercises/solution/arguments.js
@@ -81,3 +81,23 @@ Function.prototype.curry1 = function (numArgs) {
   }
   return _curriedFn;
 };
+
+/**
+ * With ES6 arrow functions
+ * Notice we dont need to keep track of the `this` context (const fn = this).
+ * An arrow function does not have its own `this`, 
+ * the `this` value of the enclosing execution context is used.
+ */
+Function.prototype.curry2 = function (nArg) {
+  const argArray = [];
+  const _curriedFn = (arg) => {
+    argArray.push(arg);
+    if (argArray.length === nArg) {
+      // spreading the array into individual arguments
+      return this(...argArray); 
+    } else {
+      return _curriedFn;
+    }
+  };
+  return _curriedFn;
+};
