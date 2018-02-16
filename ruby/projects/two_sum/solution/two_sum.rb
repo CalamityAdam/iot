d3face2e2261946cb600d1b4b17d816b7af87e2d
Isak Mladenoff <imladenoff@gmail.com>
2018-02-15 16:09:56 -0800

    CUR-57 Update four_sum solution

--- ruby/projects/two_sum/solution/two_sum.rb
+++ ruby/projects/two_sum/solution/two_sum.rb
@@ -70,60 +70,38 @@ end
 # O(n^2) time complexity
 # O(n^2) space complexity
 
-def four_sum?(arr, target_sum)
+def four_sum?(array, target)
 
-  pairs = Hash.new
+  # the first hash contains each of the elements that we have checked thus far
+  hash = Hash.new
 
-  # pairs: keys are sums, values are hashes of composites pairs[sum]: keys are
-  # composite values, values are sets of locations where values appear
+  # two_sum_hash contains sums of two elements from our array
+  two_sum_hash = Hash.new
 
-  arr.each_with_index do |a, first|
-    arr.each_with_index do |b, second|
+  # three_sum_hash contains sums of three elements from our array
+  three_sum_hash = Hash.new
 
-      next unless first < second
+  array.each do |num|
 
-      sum = [a + b].reduce(:+)
+    # Return true if there is a sum of three numbers equal to the difference of 
+    # the target and the current num (i.e., if the current num and one of the
+    # three_sums sum to the target)
+    return true if three_sum_hash[target - num]
 
-      # initializes the data structures if not already done
-      pairs[sum] ||= Hash.new
-      pairs[sum][a] ||= Set.new
-      pairs[sum][b] ||= Set.new
-
-      # stores the indices that contain `a` and `b` values, the first two numbers of a possible foursome
-      pairs[sum][a] << first
-      pairs[sum][b] << second
-
-      # finds the complement required to complete the sum
-      complement = target_sum - sum
-
-      # moves on unless there is a pair that sums to the complement-sum
-      next unless pairs[complement]
-
-      # iterates over the complement-pairs, looking for possible values `c` that can be the third number in the foursome
-
-      pairs[complement].each do |c, thirds|
-        # skip if no unused indices of the required value `c` remain
-        next if (thirds - [first, second]).empty?
-
-        # find the last required value `d` to complete the foursome
-        d = complement - c
-
-        # iterate over the thirds to find a fourth that is distinct from first, second, and third
-        (thirds - [first, second]).each do |third|
-          return true unless (pairs[complement][d] - [first, second, third]).empty?
-        end
-
-      end
-
-      # The loops above run whenever there is a possible complementary pair. We
-      # need to confirm that there is no overlap between the original pair of
-      # `a`, `b` and the complementary pair `c`, `d`. Using sets lets us exclude
-      # `a`, `b`, and `c` in turn from being used as `d` in constant time.
-      # Because these checks happen as the pairs as forming, we stop checking as
-      # soon as we find a match, avoiding having to look at all the permutations
-      # in the worst case scenario of a homogenous array of target_sum/4 values.
-
+    # if we have any nums in our two_sum hash, add the current num to each to 
+    # populate a hash of three_sums
+    two_sum_hash.each_key do |key|
+      three_sum_hash[key + num] = true
     end
+
+    # if we have any nums in our single num hash, sum the current num 
+    # with each of these numbers and store them in a hash of possible two sums
+    hash.each_key do |key|
+      two_sum_hash[key + num] = true
+    end
+
+    # lastly, add the num to a hash of all nums
+    hash[num] = true
   end
 
   false
