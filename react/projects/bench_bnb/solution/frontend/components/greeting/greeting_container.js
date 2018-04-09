41fec4b097fe2dec230bca17243cea7048a04167
Maurice Roach <moeroach94@gmail.com>
2018-04-09 17:53:37 -0400

    CUR-334 Add users slice of state

--- react/projects/bench_bnb/solution/frontend/components/greeting/greeting_container.js
+++ react/projects/bench_bnb/solution/frontend/components/greeting/greeting_container.js
@@ -3,9 +3,11 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import Greeting from './greeting';
 
-const mapStateToProps = ({ session }) => ({
-  currentUser: session.currentUser
-});
+const mapStateToProps = ({ session, entities: { users } }) => {
+  return {
+    currentUser: users[session.id]
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout())
