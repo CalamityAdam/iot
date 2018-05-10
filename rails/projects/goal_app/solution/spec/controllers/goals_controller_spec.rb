5a5540b06129e6f2d422e1fee26c5fad98e5d051
Cynthia Ma <cynthia.ma.emily@gmail.com>
2018-05-10 16:18:59 -0700

    Add GoalsController specs which allows the controller to receive the method current user

---
+++ rails/projects/goal_app/solution/spec/controllers/goals_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe GoalsController, type: :controller do
+    let(:jack) { User.create!(username: 'jack_bruce', password: 'abcdef') }
+  
+    before(:each) do
+      allow_message_expectations_on_nil
+    end
+  
+    describe 'GET #new' do
+      context 'when logged in' do
+        before do
+          allow(controller).to receive(:current_user) { jack }
+        end
+  
+        it 'renders the new Goals page' do
+          get :new
+          expect(response).to render_template('new')
+        end
+      end
+  
+      context 'when logged out' do
+        before do
+          allow(controller).to receive(:current_user) { nil }
+        end
+  
+        it 'redirects to the login page' do
+          get :new
+          expect(response).to redirect_to(new_user_url)
+        end
+      end
+    end
+end  
