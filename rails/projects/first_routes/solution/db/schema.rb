ce14aa884e3869e094bfe338c0b9540865f100f0
Matthias Jenny <matthiascjenny@gmail.com>
2018-06-27 17:01:45 -0400

    CUR-471 Switch first_routes solutions to Rails 5.2.0

--- rails/projects/first_routes/solution/db/schema.rb
+++ rails/projects/first_routes/solution/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714210134) do
+ActiveRecord::Schema.define(version: 2017_07_14_210134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
